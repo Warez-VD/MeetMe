d0a741b63ecc92b9c004db5029dad4880a27953d
Warez <vlado547@outlook.com>
2017-03-12 12:37:33 +0200

    show/hide comments added

--- MeetMe/MeetMe.Web/Scripts/publication.js
+++ MeetMe/MeetMe.Web/Scripts/publication.js
@@ -32,4 +32,26 @@ function handleFileSelect(ev) {
 }
 $(() => {
     $("#publication-picture").change(handleFileSelect);
+
+    $(".show-comments").on("click", (ev) => {
+        let target = $(ev.target);
+        let publicationId = target.attr("data-info");
+        $(`#comments-${publicationId}`).children().removeClass("hidden");
+        $(`#show-${publicationId}`).addClass("hidden");
+        $(`#hide-${publicationId}`).removeClass("hidden");
+    });
+
+    $(".hide-comments").on("click", (ev) => {
+        let target = $(ev.target);
+        let publicationId = target.attr("data-info");
+        let comments = $(`#comments-${publicationId}`).children();
+        for (var i = 0; i < comments.length; i += 1) {
+            if (i > 1) {
+                $(comments[i]).addClass("hidden");
+            }
+        }
+
+        $(`#hide-${publicationId}`).addClass("hidden");
+        $(`#show-${publicationId}`).removeClass("hidden");
+    });
 });
