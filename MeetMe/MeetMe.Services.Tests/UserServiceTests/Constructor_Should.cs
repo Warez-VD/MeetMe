35e0b6d4972dc8359d8b402ab07a010d5a5d87f9
Warez <vlado547@outlook.com>
2017-03-13 21:35:15 +0200

    added user service tests

---
+++ MeetMe/MeetMe.Services.Tests/UserServiceTests/Constructor_Should.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+
+namespace MeetMe.Services.Tests.UserServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowsWhen_UserRepositoryIsNull()
+        {
+            // Arrange & Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new UserService(null));
+
+            // Assert
+            Assert.That(ex.Message.Contains("UserRepository"));
+        }
+
+        [Test]
+        public void ReturnInstanceOfStatisticService_Correct()
+        {
+            // Arrange
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+
+            // Act
+            var userService = new UserService(mockedUserRepository.Object);
+
+            // Assert
+            Assert.IsNotNull(userService);
+            Assert.IsInstanceOf<UserService>(userService);
+        }
+    }
+}
---
+++ MeetMe/MeetMe.Services.Tests/UserServiceTests/GetByIndentityId_Should.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Moq;
+using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+
+namespace MeetMe.Services.Tests.UserServiceTests
+{
+    [TestFixture]
+    public class GetByIndentityId_Should
+    {
+        [Test]
+        public void ReturnCorrectUser_WhenFound()
+        {
+            // Arrange
+            string searchedId = "fifth";
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser() { AspIdentityUserId = searchedId };
+            var users = new List<CustomUser>()
+            {
+                user,
+                new CustomUser() { AspIdentityUserId = "first" },
+                new CustomUser() { AspIdentityUserId = "third" }
+            }.AsQueryable();
+            mockedUserRepository.Setup(x => x.All).Returns(users);
+            var userService = new UserService(mockedUserRepository.Object);
+
+            // Act
+            var result = userService.GetByIndentityId(searchedId);
+
+            // Assert
+            Assert.AreSame(result, user);
+        }
+
+        [Test]
+        public void ReturnNull_WhenUserNotFound()
+        {
+            // Arrange
+            string searchedId = "fifth";
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var users = new List<CustomUser>()
+            {
+                new CustomUser() { AspIdentityUserId = "first" },
+                new CustomUser() { AspIdentityUserId = "third" }
+            }.AsQueryable();
+            mockedUserRepository.Setup(x => x.All).Returns(users);
+            var userService = new UserService(mockedUserRepository.Object);
+
+            // Act
+            var result = userService.GetByIndentityId(searchedId);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
