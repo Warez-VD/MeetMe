4e6889bb1114d6df2b40e38b21829ace372c35c9
Warez <vlado547@outlook.com>
2017-03-16 11:16:28 +0200

    added coverall packages

--- MeetMe/MeetMe.Web/Scripts/autocomplete.js
+++ MeetMe/MeetMe.Web/Scripts/autocomplete.js
@@ -3,7 +3,6 @@
 $(() => {
     let usernames = requester.getJSON("/users/usernames")
     .then((response) => {
-        console.log(response);
         $("#search-bar").autocomplete({
             source: response
         });
