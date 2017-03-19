16025f6b3e62c878cca4af5e32eeb496d4fc316d
Warez <vlado547@outlook.com>
2017-03-19 19:29:03 +0200

    added comment service tests

---
+++ MeetMe/MeetMe.Services.Tests/CommentServiceTests/Constructor_Should.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using MeetMe.Data.Contracts;
+using MeetMe.Services.Contracts;
+using MeetMe.Data.Models;
+
+namespace MeetMe.Services.Tests.CommentServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowsWhen_CommentRepositoryIsNull()
+        {
+            // Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new CommentService(
+                null,
+                mockedUnitOfWork.Object,
+                mockedCommentFactory.Object));
+
+            // Assert
+            Assert.That(ex.Message.Contains("CommentRepository"));
+        }
+
+        [Test]
+        public void ThrowsWhen_UnitOfWorkIsNull()
+        {
+            // Arrange
+            var mockedCommentRepository = new Mock<IEFRepository<Comment>>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new CommentService(
+                mockedCommentRepository.Object,
+                null,
+                mockedCommentFactory.Object));
+
+            // Assert
+            Assert.That(ex.Message.Contains("UnitOfWork"));
+        }
+
+        [Test]
+        public void ThrowsWhen_CommentFactoryIsNull()
+        {
+            // Arrange
+            var mockedCommentRepository = new Mock<IEFRepository<Comment>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new CommentService(
+                mockedCommentRepository.Object,
+                mockedUnitOfWork.Object,
+                null));
+
+            // Assert
+            Assert.That(ex.Message.Contains("CommentFactory"));
+        }
+
+        [Test]
+        public void ReturnInstanceOfAccountService_Correct()
+        {
+            // Arrange
+            var mockedCommentRepository = new Mock<IEFRepository<Comment>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+
+            // Act
+            var commentService = new CommentService(
+                mockedCommentRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedCommentFactory.Object);
+
+            // Assert
+            Assert.IsNotNull(commentService);
+            Assert.IsInstanceOf<CommentService>(commentService);
+        }
+    }
+}
---
+++ MeetMe/MeetMe.Services.Tests/CommentServiceTests/CreatePublicationComment_Should.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+using MeetMe.Services.Contracts;
+
+namespace MeetMe.Services.Tests.CommentServiceTests
+{
+    [TestFixture]
+    public class CreatePublicationComment_Should
+    {
+        [Test]
+        public void CallCommentFactory_CreateCommentOnce()
+        {
+            // Arrange
+            var mockedCommentRepository = new Mock<IEFRepository<Comment>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+            
+            var commentService = new CommentService(
+                mockedCommentRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedCommentFactory.Object);
+            string content = "some content";
+            int userId = 12;
+            DateTime date = new DateTime(2017, 3, 20);
+
+            // Act
+            commentService.CreatePublicationComment(content, userId, date);
+
+            // Assert
+            mockedCommentFactory
+                .Verify(
+                    x => x.CreateComment(
+                        It.Is<string>(c => c == content),
+                        It.Is<int>(i => i == userId),
+                        It.Is<DateTime>(d => d == date)),
+                        Times.Once);
+        }
+
+        [Test]
+        public void CallCommentRepository_AddOnce()
+        {
+            // Arrange
+            var mockedCommentRepository = new Mock<IEFRepository<Comment>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+            var comment = new Comment();
+            mockedCommentFactory
+                .Setup(
+                    x => x.CreateComment(
+                        It.IsAny<string>(),
+                        It.IsAny<int>(),
+                        It.IsAny<DateTime>()))
+                .Returns(comment);
+
+            var commentService = new CommentService(
+                mockedCommentRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedCommentFactory.Object);
+            string content = "some content";
+            int userId = 12;
+            DateTime date = new DateTime(2017, 3, 20);
+
+            // Act
+            commentService.CreatePublicationComment(content, userId, date);
+
+            // Assert
+            mockedCommentRepository.Verify(x => x.Add(It.Is<Comment>(c => c == comment)), Times.Once);
+        }
+
+        [Test]
+        public void CallUnitOfWork_CommitOnce()
+        {
+            // Arrange
+            var mockedCommentRepository = new Mock<IEFRepository<Comment>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+
+            var commentService = new CommentService(
+                mockedCommentRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedCommentFactory.Object);
+            string content = "some content";
+            int userId = 12;
+            DateTime date = new DateTime(2017, 3, 20);
+
+            // Act
+            commentService.CreatePublicationComment(content, userId, date);
+
+            // Assert
+            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnComment_Correct()
+        {
+            // Arrange
+            var mockedCommentRepository = new Mock<IEFRepository<Comment>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+            var comment = new Comment();
+            mockedCommentFactory
+                .Setup(
+                    x => x.CreateComment(
+                        It.IsAny<string>(),
+                        It.IsAny<int>(),
+                        It.IsAny<DateTime>()))
+                .Returns(comment);
+
+            var commentService = new CommentService(
+                mockedCommentRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedCommentFactory.Object);
+            string content = "some content";
+            int userId = 12;
+            DateTime date = new DateTime(2017, 3, 20);
+
+            // Act
+            var result = commentService.CreatePublicationComment(content, userId, date);
+
+            // Assert
+            Assert.AreEqual(result, comment);
+        }
+    }
+}
