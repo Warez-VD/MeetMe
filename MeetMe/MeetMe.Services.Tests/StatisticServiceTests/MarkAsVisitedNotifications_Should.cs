c5b1dc929a27876b5ff3641a9580eb1d072d9168
Warez <vlado547@outlook.com>
2017-03-19 17:33:40 +0200

    added statistic service tests

---
+++ MeetMe/MeetMe.Services.Tests/StatisticServiceTests/MarkAsVisitedNotifications_Should.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Moq;
+using MeetMe.Services.Contracts;
+using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+
+namespace MeetMe.Services.Tests.StatisticServiceTests
+{
+    [TestFixture]
+    public class MarkAsVisitedNotifications_Should
+    {
+        [Test]
+        public void CallStatisticRepository_UpdateOnce()
+        {
+            // Arrange
+            var mockedUserService = new Mock<IUserService>();
+            var user = new CustomUser() { Id = 1 };
+            mockedUserService.Setup(x => x.GetByIndentityId(It.IsAny<string>())).Returns(user);
+            var mockedStatisticRepository = new Mock<IEFRepository<Statistic>>();
+            var statistic = new Statistic() { CustomUserId = 1 };
+            var statistics = new List<Statistic>() { statistic }.AsQueryable();
+            mockedStatisticRepository.Setup(x => x.All).Returns(statistics);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedStatisticFactory = new Mock<IStatisticFactory>();
+
+            var statisticService = new StatisticService(
+                mockedUserService.Object,
+                mockedStatisticRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedStatisticFactory.Object);
+            string userId = "some-id";
+
+            // Act
+            statisticService.MarkAsVisitedNotifications(userId);
+
+            // Assert
+            mockedStatisticRepository.Verify(x => x.Update(It.Is<Statistic>(s => s == statistic && statistic.NotificationsCount == 0)), Times.Once);
+        }
+
+        [Test]
+        public void CallUnitOfWork_CommitOnce()
+        {
+            // Arrange
+            var mockedUserService = new Mock<IUserService>();
+            var user = new CustomUser() { Id = 1 };
+            mockedUserService.Setup(x => x.GetByIndentityId(It.IsAny<string>())).Returns(user);
+            var mockedStatisticRepository = new Mock<IEFRepository<Statistic>>();
+            var statistic = new Statistic() { CustomUserId = 1 };
+            var statistics = new List<Statistic>() { statistic }.AsQueryable();
+            mockedStatisticRepository.Setup(x => x.All).Returns(statistics);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedStatisticFactory = new Mock<IStatisticFactory>();
+
+            var statisticService = new StatisticService(
+                mockedUserService.Object,
+                mockedStatisticRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedStatisticFactory.Object);
+            string userId = "some-id";
+
+            // Act
+            statisticService.MarkAsVisitedNotifications(userId);
+
+            // Assert
+            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
+        }
+    }
+}
--- MeetMe/MeetMe.Services.Tests/StatisticServiceTests/RemoveNotificationStatistic_Should.cs
+++ MeetMe/MeetMe.Services.Tests/StatisticServiceTests/RemoveNotificationStatistic_Should.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using Moq;
 using MeetMe.Services.Contracts;
@@ -9,7 +12,7 @@ using MeetMe.Data.Contracts;
 namespace MeetMe.Services.Tests.StatisticServiceTests
 {
     [TestFixture]
-    public class AddNotificationStatistic_Should
+    public class RemoveNotificationStatistic_Should
     {
         [Test]
         public void CallUserService_GetByIndentityIdOnce()
@@ -32,14 +35,42 @@ namespace MeetMe.Services.Tests.StatisticServiceTests
             string userId = "some-id";
 
             // Act
-            statisticService.AddNotificationStatistic(userId);
+            statisticService.RemoveNotificationStatistic(userId);
 
             // Assert
             mockedUserService.Verify(x => x.GetByIndentityId(It.Is<string>(i => i == userId)), Times.Once);
         }
 
         [Test]
-        public void IncreasedStatistic_NotificationCountWithOne_WhenCalledWithStringId()
+        public void DecreasedStatistic_NotificationCountWithOne()
+        {
+            // Arrange
+            var mockedUserService = new Mock<IUserService>();
+            var user = new CustomUser() { Id = 1 };
+            mockedUserService.Setup(x => x.GetByIndentityId(It.IsAny<string>())).Returns(user);
+            var mockedStatisticRepository = new Mock<IEFRepository<Statistic>>();
+            var statistic = new Statistic() { CustomUserId = 1, NotificationsCount = 1 };
+            var statistics = new List<Statistic>() { statistic }.AsQueryable();
+            mockedStatisticRepository.Setup(x => x.All).Returns(statistics);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedStatisticFactory = new Mock<IStatisticFactory>();
+
+            var statisticService = new StatisticService(
+                mockedUserService.Object,
+                mockedStatisticRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedStatisticFactory.Object);
+            string userId = "some-id";
+
+            // Act
+            statisticService.RemoveNotificationStatistic(userId);
+
+            // Assert
+            Assert.AreEqual(statistic.NotificationsCount, 0);
+        }
+
+        [Test]
+        public void CallStatisticRepository_UpdateOnce()
         {
             // Arrange
             var mockedUserService = new Mock<IUserService>();
@@ -60,98 +91,14 @@ namespace MeetMe.Services.Tests.StatisticServiceTests
             string userId = "some-id";
 
             // Act
-            statisticService.AddNotificationStatistic(userId);
-
-            // Assert
-            Assert.AreEqual(statistic.NotificationsCount, 1);
-        }
-
-        [Test]
-        public void IncreasedStatistic_NotificationCountWithOne_WhenCalledWithIntId()
-        {
-            // Arrange
-            var mockedUserService = new Mock<IUserService>();
-            var user = new CustomUser() { Id = 1 };
-            mockedUserService.Setup(x => x.GetByIndentityId(It.IsAny<string>())).Returns(user);
-            var mockedStatisticRepository = new Mock<IEFRepository<Statistic>>();
-            var statistic = new Statistic() { CustomUserId = 1 };
-            var statistics = new List<Statistic>() { statistic }.AsQueryable();
-            mockedStatisticRepository.Setup(x => x.All).Returns(statistics);
-            var mockedUnitOfWork = new Mock<IUnitOfWork>();
-            var mockedStatisticFactory = new Mock<IStatisticFactory>();
-
-            var statisticService = new StatisticService(
-                mockedUserService.Object,
-                mockedStatisticRepository.Object,
-                mockedUnitOfWork.Object,
-                mockedStatisticFactory.Object);
-            int userId = 1;
-
-            // Act
-            statisticService.AddNotificationStatistic(userId);
-
-            // Assert
-            Assert.AreEqual(statistic.NotificationsCount, 1);
-        }
-
-        [Test]
-        public void CallStatisticRepository_UpdateOnce_WhenCalledWithStringId()
-        {
-            // Arrange
-            var mockedUserService = new Mock<IUserService>();
-            var user = new CustomUser() { Id = 1 };
-            mockedUserService.Setup(x => x.GetByIndentityId(It.IsAny<string>())).Returns(user);
-            var mockedStatisticRepository = new Mock<IEFRepository<Statistic>>();
-            var statistic = new Statistic() { CustomUserId = 1 };
-            var statistics = new List<Statistic>() { statistic }.AsQueryable();
-            mockedStatisticRepository.Setup(x => x.All).Returns(statistics);
-            var mockedUnitOfWork = new Mock<IUnitOfWork>();
-            var mockedStatisticFactory = new Mock<IStatisticFactory>();
-
-            var statisticService = new StatisticService(
-                mockedUserService.Object,
-                mockedStatisticRepository.Object,
-                mockedUnitOfWork.Object,
-                mockedStatisticFactory.Object);
-            string userId = "some-id";
-
-            // Act
-            statisticService.AddNotificationStatistic(userId);
+            statisticService.RemoveNotificationStatistic(userId);
 
             // Assert
             mockedStatisticRepository.Verify(x => x.Update(It.Is<Statistic>(s => s == statistic)), Times.Once);
         }
 
         [Test]
-        public void CallStatisticRepository_UpdateOnce_WhenCalledWithIntId()
-        {
-            // Arrange
-            var mockedUserService = new Mock<IUserService>();
-            var user = new CustomUser() { Id = 1 };
-            mockedUserService.Setup(x => x.GetByIndentityId(It.IsAny<string>())).Returns(user);
-            var mockedStatisticRepository = new Mock<IEFRepository<Statistic>>();
-            var statistic = new Statistic() { CustomUserId = 1 };
-            var statistics = new List<Statistic>() { statistic }.AsQueryable();
-            mockedStatisticRepository.Setup(x => x.All).Returns(statistics);
-            var mockedUnitOfWork = new Mock<IUnitOfWork>();
-            var mockedStatisticFactory = new Mock<IStatisticFactory>();
-
-            var statisticService = new StatisticService(
-                mockedUserService.Object,
-                mockedStatisticRepository.Object,
-                mockedUnitOfWork.Object,
-                mockedStatisticFactory.Object);
-            int userId = 1;
-
-            // Act
-            statisticService.AddNotificationStatistic(userId);
-
-            // Assert
-            mockedStatisticRepository.Verify(x => x.Update(It.Is<Statistic>(s => s == statistic)), Times.Once);
-        }
-
-        [Test]
-        public void CallUnitOfWork_CommitOnce_WhenCalledWithStringId()
+        public void CallUnitOfWork_CommitOnce()
         {
             // Arrange
             var mockedUserService = new Mock<IUserService>();
@@ -172,35 +119,7 @@ namespace MeetMe.Services.Tests.StatisticServiceTests
             string userId = "some-id";
 
             // Act
-            statisticService.AddNotificationStatistic(userId);
-
-            // Assert
-            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
-        }
-
-        [Test]
-        public void CallUnitOfWork_CommitOnce_WhenCalledWithIntId()
-        {
-            // Arrange
-            var mockedUserService = new Mock<IUserService>();
-            var user = new CustomUser() { Id = 1 };
-            mockedUserService.Setup(x => x.GetByIndentityId(It.IsAny<string>())).Returns(user);
-            var mockedStatisticRepository = new Mock<IEFRepository<Statistic>>();
-            var statistic = new Statistic() { CustomUserId = 1 };
-            var statistics = new List<Statistic>() { statistic }.AsQueryable();
-            mockedStatisticRepository.Setup(x => x.All).Returns(statistics);
-            var mockedUnitOfWork = new Mock<IUnitOfWork>();
-            var mockedStatisticFactory = new Mock<IStatisticFactory>();
-
-            var statisticService = new StatisticService(
-                mockedUserService.Object,
-                mockedStatisticRepository.Object,
-                mockedUnitOfWork.Object,
-                mockedStatisticFactory.Object);
-            int userId = 1;
-
-            // Act
-            statisticService.AddNotificationStatistic(userId);
+            statisticService.RemoveNotificationStatistic(userId);
 
             // Assert
             mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
