01eb2d315ea4be5017e691b8105d8633e85b2bcd
Warez <vlado547@outlook.com>
2017-03-22 21:23:59 +0200

    added account service tests

---
+++ MeetMe/MeetMe.Services.Tests/AccountServiceTests/ChangeProfileImage_Should.cs
@@ -0,0 +1,139 @@
+﻿using System.IO;
+using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+using MeetMe.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace MeetMe.Services.Tests.AccountServiceTests
+{
+    [TestFixture]
+    public class ChangeProfileImage_Should
+    {
+        [Test]
+        public void CallImageService_GetByteArrayFromStreamOnce()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser() { ProfileImage = new ProfileImage() { Content = new byte[] { 1, 2 } } };
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+
+            var stream = new MemoryStream();
+            int id = 2;
+
+            // Act
+            accountService.ChangeProfileImage(stream, id);
+
+            // Assert
+            mockedImageService.Verify(x => x.GetByteArrayFromStream(It.Is<Stream>(s => s == stream)), Times.Once);
+        }
+
+        [Test]
+        public void CallUserRepository_GetByIdOnce()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser() { ProfileImage = new ProfileImage() { Content = new byte[] { 1, 2 } } };
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+
+            var stream = new MemoryStream();
+            int id = 2;
+
+            // Act
+            accountService.ChangeProfileImage(stream, id);
+
+            // Assert
+            mockedUserRepository.Verify(x => x.GetById(It.Is<int>(i => i == id)), Times.Once);
+        }
+
+        [Test]
+        public void CallUserRepository_UpdateWithUpdatedImageContent()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser() { ProfileImage = new ProfileImage() { Content = new byte[] { 1, 2 } } };
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var imageContent = new byte[] { 2, 5, 10 };
+            mockedImageService.Setup(x => x.GetByteArrayFromStream(It.IsAny<Stream>())).Returns(imageContent);
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+
+            var stream = new MemoryStream();
+            int id = 2;
+
+            // Act
+            accountService.ChangeProfileImage(stream, id);
+
+            // Assert
+            mockedUserRepository.Verify(x => x.Update(It.Is<CustomUser>(u => u == user && user.ProfileImage.Content == imageContent)), Times.Once);
+        }
+
+        [Test]
+        public void CallUnitOfWork_CommitOnce()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser() { ProfileImage = new ProfileImage() { Content = new byte[] { 1, 2 } } };
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+
+            var stream = new MemoryStream();
+            int id = 2;
+
+            // Act
+            accountService.ChangeProfileImage(stream, id);
+
+            // Assert
+            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
+        }
+    }
+}
---
+++ MeetMe/MeetMe.Services.Tests/AccountServiceTests/EditProfile_Should.cs
@@ -0,0 +1,160 @@
+﻿using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+using MeetMe.Services.Contracts;
+using MeetMe.Web.Models.Profile;
+using Moq;
+using NUnit.Framework;
+
+namespace MeetMe.Services.Tests.AccountServiceTests
+{
+    [TestFixture]
+    public class EditProfile_Should
+    {
+        [Test]
+        public void CallUserRepository_GetByIdOnce()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser();
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+            int id = 1;
+            var personalInfo = new ProfilePersonalnfo() { Id = id };
+
+            // Act
+            accountService.EditProfile(personalInfo);
+
+            // Assert
+            mockedUserRepository.Verify(x => x.GetById(It.Is<int>(i => i == id)), Times.Once);
+        }
+
+        [Test]
+        public void CallUserRepository_UpdateWithUpdatedUserProperties()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser();
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+            int id = 1;
+            string firstName = "Some name";
+            string lastName = "Other name";
+            int age = 15;
+            string city = "Pekin";
+            string school = "Some school";
+            string company = "Some company";
+            var personalInfo = new ProfilePersonalnfo()
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age,
+                City = city,
+                School = school,
+                Company = company
+            };
+
+            // Act
+            accountService.EditProfile(personalInfo);
+
+            // Assert
+            mockedUserRepository
+                .Verify(
+                    x => x.Update(
+                        It.Is<CustomUser>(
+                            u => u == user &&
+                            user.FirstName == firstName &&
+                            user.LastName == lastName &&
+                            user.Age == age &&
+                            user.City == city &&
+                            user.School == school &&
+                            user.Company == company)),
+                        Times.Once);
+        }
+
+        [Test]
+        public void CallUnitOfWork_CommitOnce()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser();
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+            int id = 1;
+            var personalInfo = new ProfilePersonalnfo() { Id = id };
+
+            // Act
+            accountService.EditProfile(personalInfo);
+
+            // Assert
+            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnCorrectUser()
+        {
+            // Arrange
+            var mockedAspIdentityUserFactory = new Mock<IAspIdentityUserFactory>();
+            var mockedUserFactory = new Mock<ICustomUserFactory>();
+            var mockedUserRepository = new Mock<IEFRepository<CustomUser>>();
+            var user = new CustomUser();
+            mockedUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(user);
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedImageService = new Mock<IImageService>();
+            var mockedProfileLogoFactory = new Mock<IProfileLogoFactory>();
+
+            var accountService = new AccountService(
+                mockedAspIdentityUserFactory.Object,
+                mockedUserFactory.Object,
+                mockedUserRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedImageService.Object,
+                mockedProfileLogoFactory.Object);
+            int id = 5;
+            var personalInfo = new ProfilePersonalnfo() { Id = id };
+
+            // Act
+            var result = accountService.EditProfile(personalInfo);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<CustomUser>(result);
+        }
+    }
+}
