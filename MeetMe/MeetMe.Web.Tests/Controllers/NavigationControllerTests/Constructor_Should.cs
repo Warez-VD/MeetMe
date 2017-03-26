72730c1c7aebaf94c113bdbd9674293069eb8c6b
Warez <vlado547@outlook.com>
2017-03-27 00:35:08 +0300

    added navigation controller tests

---
+++ MeetMe/MeetMe.Web.Tests/Controllers/NavigationControllerTests/Constructor_Should.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MeetMe.Services.Contracts;
+using MeetMe.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace MeetMe.Web.Tests.Controllers.NavigationControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowsWhen_StatisticServiceIsNull()
+        {
+            // Arrange
+            var mockedMapperService = new Mock<IMapperService>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new NavigationController(
+                null,
+                mockedMapperService.Object));
+
+            // Assert
+            Assert.That(ex.Message.Contains("StatisticService"));
+        }
+
+        [Test]
+        public void ThrowsWhen_MapperServiceIsNull()
+        {
+            // Arrange
+            var mockedStatisticService = new Mock<IStatisticService>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new NavigationController(
+                mockedStatisticService.Object,
+                null));
+
+            // Assert
+            Assert.That(ex.Message.Contains("MapperService"));
+        }
+
+        [Test]
+        public void ReturnInstanceOfNavigationController_Correct()
+        {
+            // Arrange
+            var mockedStatisticService = new Mock<IStatisticService>();
+            var mockedMapperService = new Mock<IMapperService>();
+
+            // Act
+            var navigationController = new NavigationController(
+                mockedStatisticService.Object,
+                mockedMapperService.Object);
+
+            // Assert
+            Assert.IsNotNull(navigationController);
+            Assert.IsInstanceOf<NavigationController>(navigationController);
+        }
+    }
+}
---
+++ MeetMe/MeetMe.Web.Tests/Controllers/NavigationControllerTests/Index_Should.cs
@@ -0,0 +1,38 @@
+﻿using MeetMe.Data.Models;
+using MeetMe.Services.Contracts;
+using MeetMe.Web.Controllers;
+using MeetMe.Web.Models.Navigation;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace MeetMe.Web.Tests.Controllers.NavigationControllerTests
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void ReturnPartialView_IndexPartial()
+        {
+            // Arrange
+            var mockedStatisticService = new Mock<IStatisticService>();
+            var mockedMapperService = new Mock<IMapperService>();
+            var model = new StatisticViewModel();
+            mockedMapperService.Setup(x => x.MapObject<StatisticViewModel>(It.IsAny<Statistic>())).Returns(model);
+            
+            var navigationController = new NavigationController(
+                mockedStatisticService.Object,
+                mockedMapperService.Object);
+            string userId = "some-id";
+
+            // Act & Assert
+            navigationController
+                .WithCallTo(x => x.Index(userId))
+                .ShouldRenderPartialView("_IndexPartial")
+                .WithModel<StatisticViewModel>(m =>
+                {
+                    Assert.AreEqual(model, m);
+                });
+        }
+    }
+}
