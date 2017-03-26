b91816c8cd68cdb02ea23168fa6f818fb06a4385
Warez <vlado547@outlook.com>
2017-03-26 19:17:20 +0300

    added users controller tests

---
+++ MeetMe/MeetMe.Web.Tests/Controllers/UsersControllerTests/Constructor_Should.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MeetMe.Services.Contracts;
+using MeetMe.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace MeetMe.Web.Tests.Controllers.UsersControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowsWhen_UserServiceIsNull()
+        {
+            // Arrange & Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new UsersController(null));
+
+            // Assert
+            Assert.That(ex.Message.Contains("UserService"));
+        }
+
+        [Test]
+        public void ReturnInstanceOfUsersController_Correct()
+        {
+            // Arrange
+            var mockedUserService = new Mock<IUserService>();
+
+            // Act
+            var usersController = new UsersController(mockedUserService.Object);
+
+            // Assert
+            Assert.IsNotNull(usersController);
+            Assert.IsInstanceOf<UsersController>(usersController);
+        }
+    }
+}
---
+++ MeetMe/MeetMe.Web.Tests/Controllers/UsersControllerTests/Usernames_Should.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using MeetMe.Services.Contracts;
+using MeetMe.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace MeetMe.Web.Tests.Controllers.UsersControllerTests
+{
+    [TestFixture]
+    public class Usernames_Should
+    {
+        [Test]
+        public void CallUserService_GetUsernamesOnce()
+        {
+            // Arrange
+            var mockedUserService = new Mock<IUserService>();
+            
+            var usersController = new UsersController(mockedUserService.Object);
+
+            // Act
+            usersController.Usernames();
+
+            // Assert
+            mockedUserService.Verify(x => x.GetUsernames(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnResultAsJson()
+        {
+            // Arrange
+            var mockedUserService = new Mock<IUserService>();
+            var usernames = new List<string>() { "first", "second" };
+            mockedUserService.Setup(x => x.GetUsernames()).Returns(usernames);
+
+            var usersController = new UsersController(mockedUserService.Object);
+
+            // Act & Assert
+            usersController.WithCallTo(x => x.Usernames())
+                .ShouldReturnJson();
+        }
+    }
+}
