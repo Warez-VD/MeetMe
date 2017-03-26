c4a9bef30d2855f2ff7ba547060804c0013d900a
Warez <vlado547@outlook.com>
2017-03-26 15:01:24 +0300

    added message service tests

---
+++ MeetMe/MeetMe.Services.Tests/MessageServiceTests/Constructor_Should.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+using MeetMe.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace MeetMe.Services.Tests.MessageServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowsWhen_MessageRepositoryIsNull()
+        {
+            // Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new MessageService(
+                null,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object));
+
+            // Assert
+            Assert.That(ex.Message.Contains("MessageRepository"));
+        }
+
+        [Test]
+        public void ThrowsWhen_UnitOfWorkIsNull()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new MessageService(
+                mockedMessageRepository.Object,
+                null,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object));
+
+            // Assert
+            Assert.That(ex.Message.Contains("UnitOfWork"));
+        }
+
+        [Test]
+        public void ThrowsWhen_MessageFactoryIsNull()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                null,
+                mockedDateTimeService.Object));
+
+            // Assert
+            Assert.That(ex.Message.Contains("MessageFactory"));
+        }
+
+        [Test]
+        public void ThrowsWhen_DateTimeServiceIsNull()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(() => new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                null));
+
+            // Assert
+            Assert.That(ex.Message.Contains("DateTimeService"));
+        }
+
+        [Test]
+        public void ReturnInstanceOfAccountService_Correct()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+
+            // Act
+            var messageService = new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object);
+
+            // Assert
+            Assert.IsNotNull(messageService);
+            Assert.IsInstanceOf<MessageService>(messageService);
+        }
+    }
+}
---
+++ MeetMe/MeetMe.Services.Tests/MessageServiceTests/CreateMessage_Should.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using MeetMe.Data.Contracts;
+using MeetMe.Data.Models;
+using MeetMe.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace MeetMe.Services.Tests.MessageServiceTests
+{
+    [TestFixture]
+    public class CreateMessage_Should
+    {
+        [Test]
+        public void CallDateTimeService_GetCurrentDateOnce()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+            var date = new DateTime(2017, 2, 3);
+            mockedDateTimeService.Setup(x => x.GetCurrentDate()).Returns(date);
+            
+            var messageService = new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object);
+            var user = new CustomUser();
+            string content = "some content";
+
+            // Act
+            messageService.CreateMessage(user, content);
+
+            // Assert
+            mockedDateTimeService.Verify(x => x.GetCurrentDate(), Times.Once);
+        }
+
+        [Test]
+        public void CallMessageFactory_CreateMessageOnce()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+            var date = new DateTime(2017, 2, 3);
+            mockedDateTimeService.Setup(x => x.GetCurrentDate()).Returns(date);
+
+            var messageService = new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object);
+            var user = new CustomUser();
+            string content = "some content";
+
+            // Act
+            messageService.CreateMessage(user, content);
+
+            // Assert
+            mockedMessageFactory.Verify(x => x.CreateMessage(content, user, date), Times.Once);
+        }
+
+        [Test]
+        public void CallMessageRepository_AddOnce_WithCreatedMessage()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var message = new Message();
+            mockedMessageFactory.Setup(x => x.CreateMessage(It.IsAny<string>(), It.IsAny<CustomUser>(), It.IsAny<DateTime>())).Returns(message);
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+            var date = new DateTime(2017, 2, 3);
+            mockedDateTimeService.Setup(x => x.GetCurrentDate()).Returns(date);
+
+            var messageService = new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object);
+            var user = new CustomUser();
+            string content = "some content";
+
+            // Act
+            messageService.CreateMessage(user, content);
+
+            // Assert
+            mockedMessageRepository.Verify(x => x.Add(message), Times.Once);
+        }
+
+        [Test]
+        public void CallUnitOfWork_CommitOnce()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+            var date = new DateTime(2017, 2, 3);
+            mockedDateTimeService.Setup(x => x.GetCurrentDate()).Returns(date);
+
+            var messageService = new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object);
+            var user = new CustomUser();
+            string content = "some content";
+
+            // Act
+            messageService.CreateMessage(user, content);
+
+            // Assert
+            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnCreatedMessage_ByTheMessageFactory()
+        {
+            // Arrange
+            var mockedMessageRepository = new Mock<IEFRepository<Message>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedMessageFactory = new Mock<IMessageFactory>();
+            var message = new Message();
+            mockedMessageFactory.Setup(x => x.CreateMessage(It.IsAny<string>(), It.IsAny<CustomUser>(), It.IsAny<DateTime>())).Returns(message);
+            var mockedDateTimeService = new Mock<IDateTimeService>();
+            var date = new DateTime(2017, 2, 3);
+            mockedDateTimeService.Setup(x => x.GetCurrentDate()).Returns(date);
+
+            var messageService = new MessageService(
+                mockedMessageRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedMessageFactory.Object,
+                mockedDateTimeService.Object);
+            var user = new CustomUser();
+            string content = "some content";
+
+            // Act
+            var result = messageService.CreateMessage(user, content);
+
+            // Assert
+            Assert.AreEqual(message, result);
+        }
+    }
+}
